8e59206f61b5683e25358272c44e94833435476a
Emil Sariyev <esariyev20557@ada.edu.az>
2026-02-17 10:52:35 +0400

    Add files via upload

---
+++ Anagram.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class Anagram {
+
+    public static boolean areAnagrams(String str1, String str2) {
+        str1 = str1.replaceAll("\\s", "").toLowerCase();
+        str2 = str2.replaceAll("\\s", "").toLowerCase();
+
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        return Arrays.equals(arr1, arr2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(areAnagrams("java", "ajva"));
+        System.out.println(areAnagrams("hello", "world"));
+    }
+}
